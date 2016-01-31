dd6ff6945bc5360e54c0b88d4fb41d2de0b23d13
Vivek <vivek.patani@somaiya.edu>
2016-01-31 02:50:26 -0500

    Added Distances

--- read-test/dict_data.py
+++ read-test/dict_data.py
@@ -1,22 +1,28 @@
 import json
+import io
 import collections
+from pprint import pprint
 
-data = open("testdata")
-# load the data from the file
-print(data)
+def main():
+    data_dump()
 
-#data_list = data.split("\n")
-data_dict = {}
-for line in data:
-    line_split_list = line.split()
-    if line_split_list[0] in data_dict:
-        data_dict[line_split_list[0]][line_split_list[1]] = line_split_list[2]
-    else:
-        data_dict[line_split_list[0]] = {line_split_list[1]:line_split_list[2]}
+user = 943
+def data_dump():
+    data = open('testdata')
+    
+    #data_list = data.split("\n")
+    data_dict = {}
+    for line in data:
+        line_split_list = line.split()
+        if line_split_list[0] in data_dict:
+            data_dict[line_split_list[0]][line_split_list[1]] = int(line_split_list[2])
+        else:
+            data_dict[line_split_list[0]] = {line_split_list[1]:int(line_split_list[2])}
 
-od = collections.OrderedDict(sorted(data_dict.items()))        
-with open("sampleOutput.txt","w") as outfile:
-    json.dump(data_dict,outfile,indent=4)
 
-print (json.dumps(data_dict,indent=4))
-data.close()
+    #od = collections.OrderedDict(sorted(data_dict.items()))
+    with open("udata.json","w") as outfile:
+        json.dump(data_dict,outfile,ensure_ascii=False,indent=4)
+
+if __name__ == '__main__':
+    main()
