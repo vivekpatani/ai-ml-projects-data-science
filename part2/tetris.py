7d41f94d44eb36f1802d572d318e20b8e6d3aa0d
David Crandall <djcran@indiana.edu>
2016-09-30 13:26:37 -0400

    adding skeleton code

---
+++ part2/tetris.py
@@ -0,0 +1,88 @@
+# Simple tetris program! v0.1
+# D. Crandall, Sept 2016
+
+from AnimatedTetris import *
+from SimpleTetris import *
+from kbinput import *
+import time, sys
+
+class HumanPlayer:
+    def get_moves(self, piece, board):
+        print "Type a sequence of moves using: \n  b for move left \n  m for move right \n  n for rotation\nThen press enter. E.g.: bbbnn\n"
+        moves = raw_input()
+        return moves
+
+    def control_game(self, tetris):
+        while 1:
+            c = get_char_keyboard()
+            commands =  { "b": tetris.left, "n": tetris.rotate, "m": tetris.right, " ": tetris.down }
+            commands[c]()
+
+#####
+# This is the part you'll want to modify!
+# Replace our super simple algorithm with something better
+#
+class ComputerPlayer:
+    # Given a new piece (encoded as a list of strings) and a board (also list of strings), 
+    # this function should generate a series of commands to move the piece into the "optimal"
+    # position. The commands are a string of letters, where b and m represent left and right, respectively,
+    # and n rotates. 
+    #
+    def get_moves(self, piece, board):
+        # super simple current algorithm: just randomly move left, right, and rotate a few times
+        return random.choice("mnb") * random.randint(1, 10)
+       
+    # This is the version that's used by the animted version. This is really similar to get_moves,
+    # except that it runs as a separate thread and you should access various methods and data in
+    # the "tetris" object to control the movement. In particular:
+    #   - tetris.col, tetris.row have the current column and row of the upper-left corner of the 
+    #     falling piece
+    #   - tetris.get_piece() is the current piece, tetris.get_next_piece() is the next piece after that
+    #   - tetris.left(), tetris.right(), tetris.down(), and tetris.rotate() can be called to actually
+    #     issue game commands
+    #   - tetris.get_board() returns the current state of the board, as a list of strings.
+    #
+    def control_game(self, tetris):
+        # another super simple algorithm: just move piece to the least-full column
+        while 1:
+            time.sleep(0.1)
+
+            board = tetris.get_board()
+            column_heights = [ min([ r for r in range(len(board)-1, 0, -1) if board[r][c] == "x"  ] + [100,] ) for c in range(0, len(board[0]) ) ]
+            index = column_heights.index(max(column_heights))
+
+            if(index < tetris.col):
+                tetris.left()
+            elif(index > tetris.col):
+                tetris.right()
+            else:
+                tetris.down()
+
+
+###################
+#### main program
+
+(player_opt, interface_opt) = sys.argv[1:3]
+
+try:
+    if player_opt == "human":
+        player = HumanPlayer()
+    elif player_opt == "computer":
+        player = ComputerPlayer()
+    else:
+        print "unknown player!"
+
+    if interface_opt == "simple":
+        tetris = SimpleTetris()
+    elif interface_opt == "animated":
+        tetris = AnimatedTetris()
+    else:
+        print "unknown interface!"
+
+    tetris.start_game(player)
+
+except EndOfGame as s:
+    print "\n\n\n", s
+
+
+
