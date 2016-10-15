1ba4bfb9c29cabcb67ed852860f6ec0e8f49972e
Vivek Patani <vivek.patani@somaiya.edu>
2016-10-15 11:56:06 -0400

    Added Row Col Evaluation

--- part1/n-k-coh-coh.py
+++ part1/n-k-coh-coh.py
@@ -2,31 +2,75 @@ import sys
 import numpy as np
 import copy
 
-#Global Variable h for height of treee
+#Global Variable h for height of tree
 h = 100
+
+# Assumptions:
+# 1 - White
+# 2 - Black
 
 class State(object):
 	height = 0
-	
+	parent = None
+
 	def __init__ (self, board, minormax):
 		self.board = board
 		self.minormax = minormax
 
-	def evaluation():
-		return len(self.board)
+	def evaluation(self, playing_for):
+		"""
+		Evaluation function for the current state.
+		"""
+		cost = 0
 
-	def is_goal():
-		return True
+		# Row Wise Scanning
+		for each_row in range(len(self.board)):
+			current_row = self.board[each_row]
+			cost += self.row_cost(current_row, playing_for)
+
+		for each_column in range(len(self.board)):
+			current_col = self.board[:,each_column]
+			cost += self.row_cost(current_col, playing_for)
+
+		return cost
+
+	def row_cost(self, current_row, playing_for):
+		
+		element_iterator = 0
+		cost = 0
+		while element_iterator < (len(current_row) - 1):
+			# Find continous runs of the players playing for
+			if (current_row[element_iterator] == playing_for and current_row[element_iterator + 1] == playing_for):
+				power = 1
+				penalty = 1
+				while (element_iterator < (len(current_row) - 1) and (current_row[element_iterator] == current_row[element_iterator + 1])):
+					penalty = 10**power
+					power += 1
+					element_iterator += 1
+				cost += penalty
+			# Else if simply add single penalty to cost
+			elif current_row[element_iterator] == playing_for:
+				cost += 1
+				element_iterator += 1
+
+			# Else just iterate through
+			else: element_iterator += 1
+
+		return cost
 
 	def game_over(self):
-
+		"""
+		Is the game over yet? Is the board full?
+		"""
 		if (np.count_nonzero(self.board) == ((len(self.board) * len(self.board)))):
 			print(self.board)
 			return True
 		return False
 
 	def get_successors(self, playing_for):
-
+		"""
+		Get the successor from the current state based on player.
+		"""
 		successor_list = []
 		for row in range(len(self.board)):
 			for column in range(len(self.board[row])):
@@ -66,9 +110,12 @@ def get_input():
 	return int(n), int(k), board_state_string, convert_to_matrix(board_state_string, int(n)), int(time)
 
 def build_tree(current_state, height, playing_for, leaves):
-
+	"""
+	Build the tree recursively.
+	"""
 	if (current_state.game_over()):
 		leaves.append(current_state)
+		print(current_state.evaluation(playing_for))
 		return leaves, current_state
 
 	successors = current_state.get_successors(playing_for)
@@ -78,10 +125,13 @@ def build_tree(current_state, height, playing_for, leaves):
 	else: playing_for = 2
 
 	for each_successor in successors:
-			build_tree(each_successor, height+1, playing_for, leaves)
+		each_successor.parent = current_state
+		build_tree(each_successor, height+1, playing_for, leaves)
 
 def main():
+	"""
 
+	"""
 	# Get Details about the board
 	n, k, board_state_string, board_state, time = get_input()
 	start = State(board_state,"max")
