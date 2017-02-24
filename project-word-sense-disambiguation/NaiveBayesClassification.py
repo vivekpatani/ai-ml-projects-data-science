d0a09762c5d06b3b0887101b8d0cf2bf59f188f5
rraavi <raghuveer.raavi@gmail.com>
2017-02-23 20:45:53 -0500

    NaiveBayes v1.1

--- project-word-sense-disambiguation/NaiveBayesClassification.py
+++ project-word-sense-disambiguation/NaiveBayesClassification.py
@@ -16,7 +16,6 @@ def generateFP(data1, data2):
     global totalClass1, Class1P
     global totalClass2, Class2P
 
-
     #Probability that text belongs to either Class1 or Class2
     Class1P = len(data1)/(len(data1)+len(data2))
     Class2P = len(data2)/(len(data1)+len(data2))
@@ -58,7 +57,7 @@ def predict(data, ClassFP, totalClassTokens,ClassP):
     for token in resultTokens:
         result += log(ClassFP.get(token,defaultClassP),2)
     result += log(ClassP)
-
+    print(result)
     return result
 
 def result(testDataPrediction_1,testDataPrediction_2):
@@ -76,5 +75,6 @@ if __name__ == "__main__":
     testData1Prediction_1 = predict(testData1, Class1FP, totalClass1, Class1P)
     testData1Prediction_2 = predict(testData1, Class2FP, totalClass2, Class2P)
     result(testData1Prediction_1, testData1Prediction_2)
-    testData2Prediction = predict(testData2, Class2FP, totalClass2, Class2P)
+    testData2Prediction_1 = predict(testData2, Class1FP, totalClass1, Class1P)
+    testData2Prediction_2 = predict(testData2, Class2FP, totalClass2, Class2P)
     result(testData1Prediction_1, testData1Prediction_2)
